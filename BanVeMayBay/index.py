0640d5b72748822df392c0db5af51e89d9f2c99f
hiu2711 <caohieu2711@gmail.com>
2024-04-20 13:15:25 +0700

    trang chủ

--- BanVeMayBay/index.py
+++ BanVeMayBay/index.py
@@ -1,5 +1,21 @@
 from flask import render_template, request, redirect
 from BanVeMayBay import app
+import dao
 
 
+@app.route("/")
+def index():
+    categories = dao.load_categories()
+    return render_template('index.html', categories=categories)
 
+@app.route('/login', methods=['get', 'post'])
+def process_login_user():
+    if request.method.__eq__('POST'):
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    with app.app_context():
+        app.run(debug=True)
